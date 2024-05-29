788c251bf357e117dfc7f495277b1fde99ab646c
ravipandya <ravi.pandya@transflo.com>
2024-05-29 18:04:07 -0400

    - Add LazyVerticalGrid and implement binding.

--- androidApp/src/main/java/com/example/movieappkmm/android/home/HomeScreen.kt
+++ androidApp/src/main/java/com/example/movieappkmm/android/home/HomeScreen.kt
@@ -1,9 +1,36 @@
 package com.example.movieappkmm.android.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.trace
 import com.example.movieappkmm.domain.model.Movie
+import okhttp3.internal.notify
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
@@ -11,5 +38,56 @@ fun HomeScreen(
     loadNextMovies: (Boolean) -> Unit,
     navigateToDetail: (Movie) -> Unit
 ) {
+    val pullRefreshState = rememberPullRefreshState(refreshing = uiState.refresh,
+        onRefresh = { loadNextMovies(true) }
+    )
 
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.background)
+            .pullRefresh(state = pullRefreshState)
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            itemsIndexed(
+                uiState.movies,
+                key = { _, movie -> movie.id }
+            ) { index, movie ->
+                MovieListItem(movie = movie, onMovieClick = {
+                    navigateToDetail(movie)
+                })
+
+                if (index > uiState.movies.size - 1 && !uiState.loading && !uiState.loadFinished) {
+                    LaunchedEffect(key1 = Unit, block = { loadNextMovies(false) })
+                }
+
+                if (uiState.loading && uiState.movies.isNotEmpty()) {
+                    this@LazyVerticalGrid.item(span = { GridItemSpan(2) }) {
+                        Row(
+                            modifier = modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            CircularProgressIndicator(
+                                color = Red
+                            )
+                        }
+
+                    }
+                }
+            }
+        }
+        PullRefreshIndicator(
+            refreshing = uiState.refresh,
+            state = pullRefreshState,
+            modifier = modifier.align(Alignment.TopCenter)
+        )
+    }
 }
