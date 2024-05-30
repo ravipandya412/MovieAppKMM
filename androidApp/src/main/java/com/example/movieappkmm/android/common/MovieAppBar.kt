82a5fe283c64e7429ad628f52b2ed5a176253e1e
ravipandya <ravi.pandya@transflo.com>
2024-05-30 17:57:41 -0400

    - Implement MovieAppBar.

---
+++ androidApp/src/main/java/com/example/movieappkmm/android/common/MovieAppBar.kt
@@ -0,0 +1,60 @@
+package com.example.movieappkmm.android.common
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+
+fun MovieAppBar(
+    modifier: Modifier = Modifier,
+    canNavigationBack: Boolean,
+    currentScreen: Destination,
+    onNavigateBack: () -> Unit
+) {
+    Surface(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp),
+        elevation = 4.dp,
+        color = MaterialTheme.colorScheme.primary
+    ) {
+        Row(
+            modifier = modifier.padding(start = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AnimatedVisibility(visible = canNavigationBack) {
+                IconButton(onClick = onNavigateBack) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+                Spacer(modifier = modifier.width(24.dp))
+            }
+        }
+
+        Text(
+            text = currentScreen.title,
+            style = MaterialTheme.typography.titleMedium,
+            modifier = modifier.padding(12.dp),
+            color = MaterialTheme.colorScheme.onSurface
+        )
+    }
+}
