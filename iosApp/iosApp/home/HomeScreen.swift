074849bad8ee1450e37151e605c506e61bc55671
ravipandya <ravi.pandya@transflo.com>
2024-07-18 17:36:20 -0400

    Implement HomeScreen.swift

--- iosApp/iosApp/home/HomeScreen.swift
+++ iosApp/iosApp/home/HomeScreen.swift
@@ -2,9 +2,30 @@ import SwiftUI
 import shared
 
 struct HomeScreen: View {
+    
+@StateObject var viewModel = HomeViewModel()
+    
+    let gridColumns : [GridItem] = Array(repeating: GridItem(.flexible(), spacing: 16), count: 2)
 
 	var body: some View {
-		Text("Hello World")
+        NavigationStack {
+            
+            ScrollView{
+                
+                LazyVGrid(columns: gridColumns, spacing: 16) {
+                    
+                    ForEach(viewModel.movies, id: \.id){movie in
+                        
+                        MovieGridItem(movie: movie)
+                    }
+                }
+                .padding(.horizontal, 12)
+            }
+            .navigationTitle("Movies")
+        }
+        .task {
+            await viewModel.loadMovies()
+        }
 	}
 }
 
